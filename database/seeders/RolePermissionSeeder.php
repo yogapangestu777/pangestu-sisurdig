1cf8d0cbb3bdd8a5b1a47a4b1906f9dd77e23c51
Yoga Pangestu <ypangestu976@gmail.com>
2025-01-17 22:57:20 +0700

    feat:install dan konfigurasi laravel spatie dan menyesuaikan seeder nya

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Biography;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'Kemahasiswaan']);
+        Permission::create(['name' => 'read_overview']);
+
+        $user = User::create([
+            'username' => 'yoga',
+            'email' => 'yoga@developer.com',
+            'password' => Hash::make(env('DEFAULT_PASSWORD_USER')),
+        ]);
+
+        $user->assignRole($user);
+
+        Biography::create([
+            'user_id' => $user->id,
+            'full_name' => 'Yoga Pangestu',
+            'phone_number' => '082121495806',
+            'pob' => 'Subang',
+            'dob' => '2005-03-13',
+            'gender' => '1',
+        ]);
+    }
+}
